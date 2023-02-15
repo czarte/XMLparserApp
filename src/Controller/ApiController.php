cf4c41cbeafab157a708c78c464b3f92c9b2f799
Vojtěch Parkán <vojtechparkan@Vojtechs-iMac.home>
2023-02-15 17:18:26 +0100

    refactorization ApiController - moving sort to separate fc

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -27,36 +27,41 @@ class ApiController extends AbstractController
             if (!$xmlFileConent['success'])
                 return new Response($xmlFileConent["message"]." <a href='$backUrl'>zpět</a>");
         } else {
-            
             return new Response("Zadejte url na zip soubor <a href='$backUrl'>zpět</a>");
         } 
 
-        $entryKey = $xmlFileConent['entryName'];
-        $response = array();
-
+        $response = $this->sortResponse(
+            $request->query->get('showprompt'), 
+            $xmlFileConent[$xmlFileConent['entryName']], 
+            $xmlFileConent["productCount"],
+        );
         $response['file_name'] = $request->query->get('url');
-
-        switch ($request->query->get('showprompt')) {
-            case 'onlycount':
-                $response['products_count'] = $xmlFileConent["productCount"];
-                break;
-            case 'onlylist':
-                foreach ($xmlFileConent[$entryKey] as $key => $entry) {
-                    if (isset($entry["parts"])) unset($xmlFileConent[$entryKey][$key]["parts"]);
-                }
-                $response['products'] = $xmlFileConent[$entryKey];
-                break;
-            case 'showparts':
-                $response['products'] = $xmlFileConent[$entryKey];
-                break;
-            default:
-                $response['products_count'] = $xmlFileConent["productCount"];
-                $response['products'] = $xmlFileConent[$entryKey];
-                break;
-        }
-
+       
         if (!empty($request->query->get('api'))) return new Response(json_encode($response));
 
         return $this->render('project/parser.html.twig', $response);
+    }
+
+    private function sortResponse($showPrompt, $xmlFileConent, $xmlFileCount) {
+        $response = array();
+        switch ($showPrompt) {
+            case 'onlycount':
+                $response['products_count'] = $xmlFileCount;
+                break;
+            case 'onlylist':
+                foreach ($xmlFileConent as $key => $entry) {
+                    if (isset($entry["parts"])) unset($xmlFileConent[$key]["parts"]);
+                }
+                $response['products'] = $xmlFileConent;
+                break;
+            case 'showparts':
+                $response['products'] = $xmlFileConent;
+                break;
+            default:
+                $response['products_count'] = $xmlFileCount;
+                $response['products'] = $xmlFileConent;
+                break;
+        }
+        return $response;
     }
 }
